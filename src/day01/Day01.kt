422e9457af640ca95d88d1f95e27ca13e7da38f1
Piotr Karcz <piotrkarcz2000@gmail.com>
2021-12-01 14:46:39 +0100

    Provided solution for day 01

---
+++ src/day01/Day01.kt
@@ -0,0 +1,24 @@
+package day01
+
+import readInput
+
+fun main() {
+    val input = readInput("day01/Day01-part1").map { it.toInt() }
+    part1(input)
+    part2(input)
+}
+
+fun part2(input: List<Int>) {
+    val lines = input
+        .zipWithNext { prev, next -> prev + next }
+        .zip(input.drop(2)) { sumOfTwo, third -> sumOfTwo + third }
+    part1(lines)
+}
+
+fun part1(input: List<Int>) {
+    var result = 0
+    for (index in 1 until input.size) {
+        if (input[index] > input[index - 1]) result++
+    }
+    println(result)
+}
